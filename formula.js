14b5d5d69378c69fd6abc239008990ab0b2e3775
alex85k <alex85k@github.com>
2023-11-27 23:00:32 +0500

    fix

--- formula.js
+++ formula.js
@@ -21,33 +21,4 @@ class Formula {
         this.latex += add;
         temml.render(this.latex, this.elem);
     }
-
-    async setAnimated(i,j,val,color1, returnColor) {
-        const c = this.selectOne(i,j);
-        let color2 = (returnColor==="restore") ? c.getFillColors() : returnColor;
-        if (val!=null){
-            this.text(i,j).innerHTML = val;
-            if (this.cellToMatrixSync) this.v[i][j]=Number(val);
-        }
-        c.setFillColor(color1);
-        await Transitions.waitForDelay();
-        //console.log(c.node(),"OK");
-        if (color2) {
-            c.setFillColor(color2);
-            await Transitions.waitForDelay();
-        }
-    }
-
-    async changeSelection(color1, returnColor, values) {
-        let color2 = (returnColor==="restore") ? this.selection.getFillColors() : returnColor;
-        this.selection.setFillColor(color1);
-        //await this.wait(this.selection);
-        await Transitions.waitForDelay();
-        if (values!=null) this.updateSelection(values);
-        if (color2) {
-            this.selection.setFillColor((_,i)=>color2[i]);
-            await Transitions.waitForDelay();
-        }
-    }
-
 }
